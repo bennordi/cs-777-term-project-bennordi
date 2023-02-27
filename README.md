c25b76afaf1ed614288f70e6db17a4902e42982b
bennordi <benjaminnordi@gmail.com>
2023-02-27 01:00:06 -0700

    Adding full model and result output

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Term Project for BU MET CS 777 - Big Data Analytics
 
 ## Instructions
-To run this project, you must first download the csv data from [Kaggle](https://www.kaggle.com/datasets/team-ai/spam-text-message-classification), and rename the file to `spam.csv` and move it to the directory of `noriega_termproject.py`.
+To run this project, clone it to your local machine which contains the script and `spam.csv` file containing our dataset.
 
 From there, run the following command and the data will be pulled successfully and the script will run.
 ```bash
@@ -22,11 +22,19 @@ Format table:
 | spam        | Free entry in 2 a weekly ... |
 
 ## Research Question
-This is a simple classification dataset, which we will leverage Pyspark and various libraries to analyze text data to build various machine learning models that will predict `ham or spam` based on the given text data. We will then review each model and its results. There are 5572 rows of data to analyze, and luckily the data was preformatted so that `ham/spam` are the only values in the category column, and the message column will need to be formatted into features columns for us to build models off of.
+This is a simple classification dataset, which we will leverage PySpark and various libraries to analyze text data to build various machine learning models that will predict `ham or spam` based on the given text data. We will then review each model and its results. There are 5572 rows of data to analyze, and luckily the data was preformatted so that `ham/spam` are the only values in the category column, and the message column will need to be formatted into features columns for us to build models off of. The main need for preprocessing was removing non-letters from the text, lemmatizing the text, and then vectorizing the data (with MLlib’s pipeline using Tokenizer(), StopWordsRemover(), CountVectorizer(), IDF(). I was going to use ChiSqSelector() but we learned in Assignment 5 that with larger datasets this can be very computationally expensive, and knowing that the goal of this assignment is to be scalable, I opted to not perform this selection.
 
 This data will be split into training and test sets (70/30 split) and will be seeded for consistency of the split. The classification models will be trained on the same exact training data and will be tested with the same test dataset. Then during the results, I can do some anecdotal tests for messages I create to test out our models.
 
 ## Machine Learning Model(s)
+Used 6 different machine learning models from PySpark's MLlib library, and fit the model with our training dataset, predicted on our test dataset. Output can be found on [output.md](output.md). 
+
+- Logistic Regression
+- Linear SVC
+- Naive Bayes classifier
+- Decision Tree classifier
+- Gradient-Boosted Tree classifier (GBT)
+- Random Forest Classifier
 
 ## Results
 
--- noriega_benjamin_term_project.py
+++ noriega_benjamin_term_project.py
@@ -4,16 +4,289 @@
 # Term Project
 
 import sys
+import time
 import numpy as np
 from pyspark import SparkContext
-from pyspark.sql import SparkSession 
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+from pyspark.ml.classification import LogisticRegression, LinearSVC, NaiveBayes, GBTClassifier, DecisionTreeClassifier, RandomForestClassifier
+from pyspark.mllib.evaluation import MulticlassMetrics
+from pyspark.ml.feature import CountVectorizer, IDF, StopWordsRemover, Tokenizer
+from pyspark.ml import Pipeline
+
+import spacy
+from pyspark.sql.functions import udf
+from pyspark.sql.types import StringType
+
+# Load the spaCy model
+nlp = spacy.load("en_core_web_sm", disable=["parser", "ner"])
+
+def vectorize(df):
+	"""
+	Converts our dataframe into vectorized text
+	
+	Params:
+		df - DataFrame - represents our dataset
+
+	Returns:
+		DataFrame - vectorized dataframe
+	"""
+	tokenizer = Tokenizer(inputCol="Message", outputCol="words")
+	remover = StopWordsRemover(inputCol=tokenizer.getOutputCol(), outputCol="filtered")
+	countVectorizer = CountVectorizer(inputCol=remover.getOutputCol(), outputCol="features_c", vocabSize=5000)
+	idf = IDF(inputCol=countVectorizer.getOutputCol(), outputCol="features")
+	pipeline = Pipeline(stages=[tokenizer, remover, countVectorizer, idf])
+
+	model = pipeline.fit(df)
+
+	return model.transform(df)
+
+# Taken from course Github page - Spark-Example-23-Mllib-Sentiment%20Model.ipynb
+def lemmatize_text(text):
+    doc = nlp(text)
+    lemmas = [token.lemma_ for token in doc]
+    return " ".join(lemmas)
+
+def get_metrics(cm):
+	"""
+	Calculates and prints out performance metrics for the predictions made.
+	Taken from my assignment 5 submission.
+
+	Params:
+		cm - ndarray - represents confusion matrix
+
+	Returns:
+		N/A - prints out results
+		
+	"""
+	tp = cm[0][0]
+	fp = cm[0][1]
+	fn = cm[1][0]
+	tn = cm[1][1]
+
+	try:
+		accuracy = (tp + tn) / (tp + fp + fn + tn)
+	except:
+		accuracy = 0
+	try:
+		prec = tp / (tp + fp)
+	except:
+		prec = 0
+	try:
+		rec = tp / (tp + fn)
+	except:
+		rec = 0
+	try:
+		f1 = 2 * (prec * rec) / (prec + rec)
+	except: 
+		f1 = 0
+	
+	print(f"Accuracy: {accuracy:.3f}")
+	print(f"Precision: {prec:.3f}")
+	print(f"Recall: {rec:.3f}")
+	print(f"F1: {f1:.3f}")
+	print(f"Confusion Matrix: \n{cm}")
+	return
+
+def logistic_regression(trainData, testData):
+	"""
+	Builds a logistic regression model based on our training data and predicts based on our classifier.
+
+	Params:
+		trainData - DataFrame - training portion of our dataset
+		testData  - DataFrame - test portion of our dataset
+
+	Returns:
+		N/A - result metrics are printed to the console
+	"""
+	print("-"*50)
+	print("Logistic Regression")
+	print()
+
+	lr = LogisticRegression(featuresCol="features", labelCol="label", maxIter=20)
+	model = lr.fit(trainData)
+
+	predictions = model.transform(testData)
+	predsLabels = predictions.select("prediction", "label").rdd
+	metrics = MulticlassMetrics(predsLabels)
+	get_metrics(metrics.confusionMatrix().toArray())
+
+
+def linear_svc(trainData, testData):
+	"""
+	Builds a linear SVC model based on our training data and predicts based on our classifier.
+
+	Params:
+		trainData - DataFrame - training portion of our dataset
+		testData  - DataFrame - test portion of our dataset
+
+	Returns:
+		N/A - result metrics are printed to the console
+	"""
+	print("-"*50)
+	print("Linear SVC")
+	print()
+
+	lsvc = LinearSVC(featuresCol="features", labelCol="label", maxIter=20)
+	model = lsvc.fit(trainData)
+
+	predictions = model.transform(testData)
+	predsLabels = predictions.select("prediction", "label").rdd
+	metrics = MulticlassMetrics(predsLabels)
+	get_metrics(metrics.confusionMatrix().toArray())
+
+def decision_tree_classifier(trainData, testData):
+	"""
+	Builds a decision tree classifier model based on our training data and predicts based on our classifier.
+
+	Params:
+		trainData - DataFrame - training portion of our dataset
+		testData  - DataFrame - test portion of our dataset
+
+	Returns:
+		N/A - result metrics are printed to the console
+	"""
+	print("-"*50)
+	print("Decision Tree Classifier")
+	print()
+
+	dt = DecisionTreeClassifier(featuresCol="features", labelCol="label")
+	model = dt.fit(trainData)
+
+	predictions = model.transform(testData)
+	predsLabels = predictions.select("prediction", "label").rdd
+	metrics = MulticlassMetrics(predsLabels)
+	get_metrics(metrics.confusionMatrix().toArray())
+
+def naive_bayes_classifier(trainData, testData):
+	"""
+	Builds a Naive Bayes classifier model based on our training data and predicts based on our classifier.
+
+	Params:
+		trainData - DataFrame - training portion of our dataset
+		testData  - DataFrame - test portion of our dataset
+
+	Returns:
+		N/A - result metrics are printed to the console
+	"""
+	print("-"*50)
+	print("Naive Bayes Classifier")
+	print()
+
+	nb = NaiveBayes(featuresCol="features", labelCol="label")
+	model = nb.fit(trainData)
+
+	predictions = model.transform(testData)
+	predsLabels = predictions.select("prediction", "label").rdd
+	metrics = MulticlassMetrics(predsLabels)
+	get_metrics(metrics.confusionMatrix().toArray())
+
+def gbt_classifier(trainData, testData):
+	"""
+	Builds a Gradient-Boosted tree classifier model based on our training data and predicts based on our classifier.
+
+	Params:
+		trainData - DataFrame - training portion of our dataset
+		testData  - DataFrame - test portion of our dataset
+
+	Returns:
+		N/A - result metrics are printed to the console
+	"""
+	print("-"*50)
+	print("GBT Classifier")
+	print()
+	gbt = GBTClassifier(maxIter=20, featuresCol="features", labelCol="label", maxDepth=5)
+	model = gbt.fit(trainData)
+
+	predictions = model.transform(testData)
+	predsLabels = predictions.select("prediction", "label").rdd
+	metrics = MulticlassMetrics(predsLabels)
+	get_metrics(metrics.confusionMatrix().toArray())
+
+def random_forest_classifier(trainData, testData):
+	"""
+	Builds a Random Forest classifier model based on our training data and predicts based on our classifier.
+
+	Params:
+		trainData - DataFrame - training portion of our dataset
+		testData  - DataFrame - test portion of our dataset
+
+	Returns:
+		N/A - result metrics are printed to the console
+	"""
+	print("-"*50)
+	print("Random Forest Classifier")
+	print()
+
+	rf = RandomForestClassifier(featuresCol="features", labelCol="label", numTrees=10)
+	model = rf.fit(trainData)
+
+	predictions = model.transform(testData)
+	predsLabels = predictions.select("prediction", "label").rdd
+	metrics = MulticlassMetrics(predsLabels)
+	get_metrics(metrics.confusionMatrix().toArray())
+
 
 if __name__ == "__main__":
-    print("Starting Term Project")
-    print()
+	print("Starting Term Project")
+	print()
 
-    sc = SparkContext(appName="Assignment-5")
-    spark = SparkSession.builder.getOrCreate()
+	sc = SparkContext(appName="Term Project")
+	spark = SparkSession.builder.getOrCreate()
 
-    corpus = sc.textFile(sys.argv[1], 1)
-    print(corpus.take(1))
+	################################################################
+	## Stage 1: Preparing Data
+	t = time.time()
+
+	df = spark.read.csv(sys.argv[1], inferSchema=True, header=True)
+	# Creating label column
+	df = df.withColumn("label", F.when(F.col("Category") == "spam", 1.0).otherwise(0.0))
+
+	# Remove non-letters
+	df = df.withColumn("Message", F.regexp_replace("Message", r"[^a-zA-Z ]", ""))
+
+	# Define a UDF to apply the lemmatizer to a column
+	lemmatize_udf = udf(lemmatize_text, StringType())
+
+	# Applying UDF
+	df = df.withColumn("Message", lemmatize_udf(df["Message"])).cache()
+
+	df = vectorize(df)
+	
+	# Splitting training and test data
+	trainData, testData = df.randomSplit(weights=[0.7, 0.3], seed=7335)
+
+	print(f"Time to load, preprocess and vectorize data: {time.time() - t:.3f} secs")
+	print()
+
+	################################################################
+	## Stage 2: Building Machine Learning Models - Evaluation
+
+	# Models are built and predicted in each helper method - results printed to console
+	tm = time.time()
+	logistic_regression(trainData, testData)
+	print(f"Time to build, fit, and predict: {time.time() - tm:.3f} secs")
+
+	tm = time.time()
+	linear_svc(trainData, testData)
+	print(f"Time to build, fit, and predict: {time.time() - tm:.3f} secs")
+
+	tm = time.time()
+	naive_bayes_classifier(trainData, testData)
+	print(f"Time to build, fit, and predict: {time.time() - tm:.3f} secs")
+
+	tm = time.time()
+	decision_tree_classifier(trainData, testData)
+	print(f"Time to build, fit, and predict: {time.time() - tm:.3f} secs")
+	
+	tm = time.time()
+	gbt_classifier(trainData, testData)
+	print(f"Time to build, fit, and predict: {time.time() - tm:.3f} secs")
+	
+	tm = time.time()
+	random_forest_classifier(trainData, testData)
+	print(f"Time to build, fit, and predict: {time.time() - tm:.3f} secs")
+
+	print()
+	print("-"*50)
+	print(f"Total Time: {time.time() - t:.3f} secs")
